6be022b0da65f5de2a50185b16260f68ff4d8573
abdugaffar <abdugaffarkutbiddinov@gmail.com>
2022-02-10 16:52:10 +0300

    movie_model.dart class implemented

---
+++ lib/features/movies/data/models/movie_model.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:movies/features/movies/domain/entities/movie.dart';
+
+List<MovieModel> movieListModelFromJson(String str) =>
+    List<MovieModel>.from(json.decode(str).map((x) => MovieModel.fromJson(x)));
+
+String movieListToJson(List<MovieModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class MovieModel extends Movie {
+  MovieModel(
+      {required int id,
+      required String title,
+      required String posterPath,
+      required String releaseDate,
+      required double voteAverage,
+      required String overview})
+      : super(
+            id: id,
+            title: title,
+            posterPath: posterPath,
+            releaseDate: releaseDate,
+            voteAverage: voteAverage,
+            overview: overview);
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+        id: json['id'] as int,
+        title: ['title'] as String,
+        posterPath: ['posted_path'] as String,
+        releaseDate: ['release_date'] as String,
+        voteAverage: ['vote_average'] as double,
+        overview: ['overview'] as String);
+  }
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "posted_path": posterPath,
+    "release_date": releaseDate,
+    "vote_average": voteAverage,
+    "overview": overview,
+  };
+
+  static List<MovieModel> popularMoviesFromSnapshot(List snapshot) {
+    return snapshot.map((data) {
+      return MovieModel.fromJson(data);
+    }).toList();
+  }
+}

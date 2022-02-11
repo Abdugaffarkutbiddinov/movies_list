23752a0c4ace9bb0b6e3250d163553f0ce85f174
abdugaffar <abdugaffarkutbiddinov@gmail.com>
2022-02-11 18:53:47 +0300

    email validation implemented

--- lib/features/sign_in/presentation/pages/sign_in_form.dart
+++ lib/features/sign_in/presentation/pages/sign_in_form.dart
@@ -1,16 +1,26 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/features/movies/presentation/pages/movie_list_page.dart';
-import 'package:movies/features/sign_in/validators.dart';
 
 import '../../../../injection_container.dart';
 import '../../../movies/presentation/bloc/movie_bloc.dart';
+import '../../validator.dart';
 import '../widgets/form_submit_button.dart';
 
+class EmailEditingRegexValidator extends RegexValidator {
+  EmailEditingRegexValidator()
+      : super(
+            regexSource:
+                "^[a-zA-Z0-9_.+-]*(@([a-zA-Z0-9-]*(\\.[a-zA-Z0-9-]*)?)?)?\$");
+}
 
-class SignInForm extends StatefulWidget with EmailAndPasswordValidators {
+class EmailSubmitRegexValidator extends RegexValidator {
+  EmailSubmitRegexValidator()
+      : super(
+            regexSource: "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-]+\$)");
+}
 
+class SignInForm extends StatefulWidget {
   @override
   State<SignInForm> createState() => _SignInFormState();
 }
@@ -29,27 +39,44 @@ class _SignInFormState extends State<SignInForm> {
 
   bool _submitted = false;
 
+  // String _value = '';
+
   void _submit() async {
     setState(() {
       _submitted = true;
     });
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => BlocProvider.value(value: sl<MovieBloc>(),child: MovieListPage(),),
+      builder: (context) => BlocProvider.value(
+        value: sl<MovieBloc>(),
+        child: MovieListPage(),
+      ),
     ));
   }
 
+  // void _submit() async {
+  //   bool valid = EmailSubmitRegexValidator().isValid(_email);
+  //   if (valid) {
+  //     _emailFocusNode.unfocus();
+  //     // widget.onSubmit(_email);
+  //   } else {
+  //     FocusScope.of(context).requestFocus(_emailFocusNode);
+  //   }
+  // }
+
   void _emailEditingComplete() {
-    final newFocus = widget.emailValidator.isValid(_email) ?
-    _passwordFocusNode : _emailFocusNode;
+    final newFocus = EmailEditingRegexValidator().isValid(_email)
+        ? _passwordFocusNode
+        : _emailFocusNode;
     FocusScope.of(context).requestFocus(newFocus);
   }
 
-
   List<Widget> _buildChildren() {
-    const primaryText =  'Sign in';
+    const primaryText = 'Sign in';
 
-    bool submitEnabled = widget.emailValidator.isValid(_email) &&
-        widget.passwordValidator.isValid(_password);
+    // bool submitEnabled = widget.emailValidator.isValid(_email) &&
+    //     widget.passwordValidator.isValid(_password);
+    bool submitEnabled = EmailSubmitRegexValidator().isValid(_email) &&
+        NonEmptyStringValidator().isValid(_password);
     return [
       _buildEmailTextField(),
       SizedBox(
@@ -66,13 +93,13 @@ class _SignInFormState extends State<SignInForm> {
 
   TextField _buildPasswordTextField() {
     bool showErrorText =
-        _submitted && !widget.passwordValidator.isValid(_password);
+        _submitted && !NonEmptyStringValidator().isValid(_password);
     return TextField(
       focusNode: _passwordFocusNode,
       controller: _passwordController,
       decoration: InputDecoration(
           labelText: 'Password',
-          errorText: showErrorText ? widget.invalidEmailErrorText : null),
+          errorText: showErrorText ? 'Enter at least 6 digits' : null),
       obscureText: true,
       textInputAction: TextInputAction.done,
       onEditingComplete: _submit,
@@ -81,17 +108,22 @@ class _SignInFormState extends State<SignInForm> {
   }
 
   TextField _buildEmailTextField() {
-    bool showErrorText = _submitted && !widget.emailValidator.isValid(_email);
+    // bool showErrorText = _submitted && !widget.emailValidator.isValid(_email);
+    bool showErrorText =
+        _submitted && !EmailSubmitRegexValidator().isValid(_email);
     return TextField(
       focusNode: _emailFocusNode,
       controller: _emailController,
       decoration: InputDecoration(
           labelText: 'Email',
           hintText: 'test@test.com',
-          errorText: showErrorText ? widget.invalidEmailErrorText : null),
+          errorText: showErrorText ? 'Email field invalid' : null),
       autocorrect: false,
       keyboardType: TextInputType.emailAddress,
       textInputAction: TextInputAction.next,
+      inputFormatters: [ValidatorInputFormatter(
+        editingValidator: EmailEditingRegexValidator(),
+      ),],
       onEditingComplete: _emailEditingComplete,
       onChanged: (email) => _updateState(),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:movies_list/main.dart';
+import 'package:movies/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
